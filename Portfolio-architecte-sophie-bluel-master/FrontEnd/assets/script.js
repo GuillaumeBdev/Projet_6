e5e917cc90601874a0a75374665db903e7354eae
GuillaumeBdev <votre@adresse-email.com>
2023-06-28 18:23:35 +0200

    Added 4 dynamic buttons

--- Portfolio-architecte-sophie-bluel-master/FrontEnd/assets/script.js
+++ Portfolio-architecte-sophie-bluel-master/FrontEnd/assets/script.js
@@ -1,25 +1,71 @@
-const getWorks = () => {
-  const container = document.getElementById("gallery");
-  fetch("http://localhost:5678/api/works")
-    .then(function(res) {
-      return res.json();
-    })
-    .then(function(data) {
+const fetchData = () => {
+  return fetch("http://localhost:5678/api/works")
+    .then(res => res.json())
+    .then(data => {
       console.log(data);
-      for (const works of data) {
-        const figure = document.createElement("figure");
-        const img = document.createElement("img");
-        const figcaptation = document.createElement("figcaption");
-
-        img.src = works.imageUrl;
-        img.alt = works.title;
-        figcaptation.textContent = works.title;
-
-        figure.appendChild(img);
-        figure.appendChild(figcaptation);
-        container.appendChild(figure);
-      }
+      return data;
     });
 };
 
-getWorks();
+const createElement = (work) => {
+  const figure = document.createElement("figure");
+  const img = document.createElement("img");
+  const figcaption = document.createElement("figcaption");
+
+  img.src = work.imageUrl;
+  img.alt = work.title;
+  figcaption.textContent = work.title;
+
+  figure.appendChild(img);
+  figure.appendChild(figcaption);
+
+  return figure;
+};
+
+const displayWorks = async () => {
+  const container = document.getElementById("gallery");
+  const worksData = await fetchData();
+
+  for (const work of worksData) {
+    const figure = createElement(work);
+    container.appendChild(figure);
+  }
+};
+
+displayWorks();
+
+const filtres = document.getElementById("filtres");
+
+const handleClick = async (category) => {
+  const container = document.getElementById("gallery");
+  container.innerHTML = "";
+
+  const worksData = await fetchData();
+
+  const filteredWorks = worksData.filter(work => work.category === category);
+
+  for (const work of filteredWorks) {
+    const figure = createElement(work);
+    container.appendChild(figure);
+  }
+};
+
+const button1 = document.createElement("button");
+button1.textContent = "Tous";
+button1.addEventListener("click", () => handleClick("all"));
+filtres.appendChild(button1);
+
+const button2 = document.createElement("button");
+button2.textContent = "Projet";
+button2.addEventListener("click", () => handleClick("project"));
+filtres.appendChild(button2);
+
+const button3 = document.createElement("button");
+button3.textContent = "Appartements";
+button3.addEventListener("click", () => handleClick("apartments"));
+filtres.appendChild(button3);
+
+const button4 = document.createElement("button");
+button4.textContent = "Hôtel & Restaurant";
+button4.addEventListener("click", () => handleClick("hotel_restaurant"));
+filtres.appendChild(button4);
